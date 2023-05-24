f100c4e6ca179183c829493280d9139d260fe049
Jonatas Rossetto <jonatas.rossetto@gmail.com>
2023-05-24 15:28:38 -0300

    alteração no algorítimo de codificação

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let keys = [
 
 btnEncode.addEventListener('click', (e) => {
   e.preventDefault();
-  displayTexto.innerText = encode(inputTexto.value);
+  displayTexto.innerText = encode2(inputTexto.value);
   // navigator.clipboard.writeText(displayTexto.innerText); //copia para o clipboard
 });
 
@@ -61,31 +61,57 @@ function encode(texto) {
   return encoded;
 }
 
+function encode2(texto) {
+  let encoded = '';
+  if (texto != '') {
+    for (let index = 0; index < texto.length; index++) {
+      let aux = texto[index];
+      console.log('aux: ' + aux);
+      for (let i = 0; i < keys.length; i++) {
+        if (texto[index] == keys[i].k) {
+          aux = keys[i].v;
+        }
+      }
+      console.log('aux: ' + aux);
+      encoded = encoded + aux;
+      console.log('encoded: ' + encoded);
+    }
+  } else {
+    encoded = 'texto vazio';
+  }
+  return encoded;
+}
+
 function decode(texto) {
-  let antes = '';
-  //separa todas as ocorrências de todas as partículas codificadas
-  //isso corrige a decodificação do texto codificado de palavras como caindo e ainda
-  //corrige também a decodificação de palavras codificadas mais de uma vez
-  for (let i = 0; i < keys.length; i++) {
+  if (texto != '') {
+    let antes = '';
+    //separa todas as ocorrências de todas as partículas codificadas
+    //isso corrige a decodificação do texto codificado de palavras como caindo e ainda
+    //corrige também a decodificação de palavras codificadas mais de uma vez
+    for (let i = 0; i < keys.length; i++) {
+      antes = '';
+      while (antes != texto) {
+        antes = texto;
+        texto = texto.replace(keys[i].v, keys[i].k + '*');
+      }
+    }
+    //procura o código e realiza a troca pelo texto correto
+    for (let i = 0; i < keys.length; i++) {
+      antes = '';
+      while (antes != texto) {
+        antes = texto;
+        texto = texto.replace(keys[i].v, keys[i].k);
+      }
+    }
     antes = '';
+    //remove o caracter separador *
     while (antes != texto) {
       antes = texto;
-      texto = texto.replace(keys[i].v, keys[i].k + '*');
+      texto = texto.replace('*', '');
     }
+  } else {
+    texto = 'texto vazio';
   }
-  //procura o código e realiza a troca pelo texto correto
-  for (let i = 0; i < keys.length; i++) {
-    antes = '';
-    while (antes != texto) {
-      antes = texto;
-      texto = texto.replace(keys[i].v, keys[i].k);
-    }
-  }
-  antes = '';
-  //remove o caracter separador *
-  while (antes != texto) {
-    antes = texto;
-    texto = texto.replace('*', '');
-  }
+
   return texto;
 }
